4dade0f05b4c81266ecb3ea2f0e06ce459d67c7a
Kenji2020 <83725041+Kenji2020@users.noreply.github.com>
2022-11-22 17:21:44 -0300

    primer commit

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3000;
+let users = []
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+const addUser = (userId, roomId) => {
+        users.push({ userId, roomId })
+}
+const userLeave = (userName) => {
+    return users = users.filter(user => user.userName !== userName)
+}
+
+const getRoomUsers = (roomId) => {
+    return users.filter(user => user.roomId === roomId)
+}
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('join-room', ({roomId, userName}) => {
+        console.log('user joined room');
+        console.log(roomId);
+        console.log(userName);
+        socket.join(roomId);
+        addUser(userName, roomId);
+        socket.to(roomId).emit('user-connected', userName);
+        io.to(roomId).emit('all-users', getRoomUsers(roomId));
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+            socket.leave(roomId);
+            userLeave(userName);
+            io.to(roomId).emit('all-users', getRoomUsers(roomId));
+        }
+        )
+    });
+})
+
+
+
+
+
+server.listen(port, () => {
+    console.log('Server listening at port %d', port);
+    }
+);
+
